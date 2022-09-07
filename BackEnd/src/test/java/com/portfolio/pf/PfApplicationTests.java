38c43f0074fbf0ae9ef3cde010562b122965cc19
Patricio Frascaroli <pmfrascaroli@gmail.com>
2022-09-07 15:48:08 -0300

    New Spring Project

---
+++ BackEnd/src/test/java/com/portfolio/pf/PfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.pf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

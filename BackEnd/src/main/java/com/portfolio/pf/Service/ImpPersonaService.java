c834bc286a1c5ef8687c23ed69176bc93c7a60d4
Patricio Frascaroli <pmfrascaroli@gmail.com>
2022-09-08 10:53:58 -0300

    Codificación en NetBeans

---
+++ BackEnd/src/main/java/com/portfolio/pf/Service/ImpPersonaService.java
@@ -0,0 +1,44 @@
+package com.portfolio.pf.Service;
+
+import com.portfolio.pf.Entity.Persona;
+import com.portfolio.pf.Interface.IPersonaService;
+import com.portfolio.pf.Repository.IPersonaRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ImpPersonaService implements IPersonaService{
+    @Autowired IPersonaRepository ipersonaRepository;
+    
+    @Override
+    public List<Persona> getPersona() {
+        List<Persona> persona = ipersonaRepository.findAll();
+        return persona;
+    }
+
+    /**
+     *
+     * @param persona
+     */
+    @Override
+    public void savePersona(Persona persona) {
+        ipersonaRepository.save(persona);
+    }
+
+    /**
+     *
+     * @param id
+     */
+    @Override
+    public void deletePersona(Long id) {
+        ipersonaRepository.deleteById(id);
+    }
+
+    @Override
+    public Persona findPersona(Long id) {
+        Persona persona = ipersonaRepository.findById(id) .orElse (null) ;
+        return persona;
+    }
+    
+}

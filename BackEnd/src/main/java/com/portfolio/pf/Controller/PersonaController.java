72192dfe972e2b0c76d80e3698124d7cc18ed40d
Patricio Frascaroli <pmfrascaroli@gmail.com>
2024-01-16 18:41:11 -0300

    Cambio de dominio firebase

--- BackEnd/src/main/java/com/portfolio/pf/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/portfolio/pf/Controller/PersonaController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin(origins = {"https://frontendpmf.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://argprograma-ada47.web.app","http://localhost:4200"})
 public class PersonaController {
     @Autowired
     ImpPersonaService personaService;
--- BackEnd/src/main/java/com/portfolio/pf/Security/Controller/AuthController.java
+++ BackEnd/src/main/java/com/portfolio/pf/Security/Controller/AuthController.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontendpmf.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://argprograma-ada47.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
